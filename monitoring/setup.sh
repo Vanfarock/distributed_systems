5c70487c87a0468e9d6ec8283b337eb6c3504e88
Vanfarock <vinicius.m.m.2002@gmail.com>
2022-11-18 20:30:17 -0300

    Change monitoring port forward location

--- monitoring/setup.sh
+++ monitoring/setup.sh
@@ -5,3 +5,9 @@ helm install --wait --timeout 15m \
   --namespace monitoring --create-namespace \
   --repo https://prometheus-community.github.io/helm-charts \
   monitoring kube-prometheus-stack
+
+echo "Port forward Prometheus 9000 -> 9090"
+echo "Port forward Grafana 9001 -> 80"
+echo ""
+kubectl port-forward svc/monitoring-kube-prometheus-prometheus -n monitoring 9000:9090 &\
+kubectl port-forward svc/monitoring-grafana -n monitoring 9001:80
