105e2058b42bf37d2acce1936a9858c03289c95b
Vanfarock <vinicius.m.m.2002@gmail.com>
2022-11-18 19:46:49 -0300

    Readd ingress and services to namespace

--- k8s/setup.sh
+++ k8s/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Setting up tweet-writer..."
-# kubectl create namespace tweet-writer
-kubectl apply -f ./k8s/tweet-writer/deployment.yaml # -n tweet-writer
-kubectl apply -f ./k8s/tweet-writer/service.yaml # -n tweet-writer
-kubectl apply -f ./k8s/tweet-writer/ingress.yaml # -n tweet-writer
+kubectl create namespace tweet-writer
+kubectl apply -f ./k8s/tweet-writer/deployment.yaml -n tweet-writer
+kubectl apply -f ./k8s/tweet-writer/service.yaml -n tweet-writer
+kubectl apply -f ./k8s/tweet-writer/ingress.yaml -n tweet-writer
 
 sleep 1
