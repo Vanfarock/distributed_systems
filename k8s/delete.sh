07c8d78dc789bcb511b1e102f9726461726a7943
Vanfarock <vinicius.m.m.2002@gmail.com>
2022-11-26 12:46:35 -0300

    Fix ingress name delete

--- k8s/delete.sh
+++ k8s/delete.sh
@@ -3,6 +3,6 @@
 echo "Deleting shortest-path..."
 kubectl delete deploy shortest-path -n shortest-path
 kubectl delete svc shortest-path -n shortest-path
-kubectl delete ingress shortest-path-ingress -n shortest-path
+kubectl delete ingress shortest-path -n shortest-path
 
 sleep 1
